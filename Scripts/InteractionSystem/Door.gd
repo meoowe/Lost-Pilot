4b5f5fc59d6e2c9ba0b8be46a350888ca9e118a8
ChaoticTechSupport <ChaoticTech0111@gmail.com>
2024-10-29 12:26:57 -0400

    test -  true

--- Scripts/InteractionSystem/Door.gd
+++ Scripts/InteractionSystem/Door.gd
@@ -1,7 +1,7 @@
 extends InteractableObj
 class_name InteractableDoor
 var door_state : bool = false
-@export var anim:AnimatedSprite2D
+@export var anim:AnimationPlayer
 func _ready() -> void:
 	priority = 2
 	interact()
@@ -11,9 +11,9 @@ func interact() -> void:
 	match door_state:
 		true:
 			global.pathfinder.set_point_solid(global.map.local_to_map(position), true)
-			anim.play_backwards("open")
+			anim.play_backwards("Open")
 			priority = 2
 		false:
 			global.pathfinder.set_point_solid(global.map.local_to_map(position), false)
-			anim.play("open")
+			anim.play("Open")
 			priority = 1
